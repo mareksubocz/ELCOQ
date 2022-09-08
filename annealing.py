full capabilities, wrong results

annealing.py
@@ -2,15 +2,35 @@ import pandas as pd
 import numpy as np
 from pyqubo import Binary, Base
 from neal import SimulatedAnnealingSampler
+from datasetgenerator import DatasetGenerator
 
-def instance_to_matrix(instance, nodes, stations):
-    speeds = pd.DataFrame({'speed': [60,70,80,90,100,110]})
+def instance_to_matrix(instance, nodes, stations, charging_speed):
+    dsg = DatasetGenerator(max_length=500, available_car_speeds=[60, 70], charging_speed=22)
+    length, nodes, stations_before, chargers = dsg.generate_highway(100)
+    car_info = dsg.generate_car_types(2)
+    instance = dsg.generate_car_instances(10.0, 0.1)
+
+    speeds = pd.DataFrame({'speed': dsg.available_car_speeds})
+    stations = []
+    for i, station in enumerate(stations_before):
+        stations.extend([station]*chargers[i])
+    stations = pd.DataFrame({'station_kilometer':stations})
     instance['original'] = instance.index
+
     im = instance.copy(deep=True)
-    im['entry_node'] = nodes[im['entry_node_index']]
-    im['exit_node'] = nodes[im['exit_node_index']]
-    im = im.merge(speeds, how='cross').merge(pd.DataFrame({'station':stations}), how='cross')
-    im['time_of_arrival'] = im['entry_timestamp'] + ( (im['station'] - im['entry_node']) / im['speed'] )
+    im['entry_kilometer'] = nodes[im['entry_node_index']]
+    im['exit_kilometer'] = nodes[im['exit_node_index']]
+    im = im.merge(speeds, how='cross').merge(stations, how='cross')
+    im = im[im['entry_kilometer'] <= im['station_kilometer']].reset_index()
+    im['station_timestamp'] = ((im['entry_timestamp']/3600 + ((im['station_kilometer'] - im['entry_kilometer']) / im['speed'] )) * 3600).round().astype(int)
+    burn = pd.Series(car_info.lookup(im['car_type'],im['speed']))
+    im['station_energy'] = car_info['capacity'][im['car_type']].reset_index(drop=True) * im['entry_energy']
+    im['station_energy'] -= (burn * (im['station_kilometer'] - im['entry_kilometer']) / 1000)
+    im['station_energy'] /= car_info['capacity'][im['car_type']].reset_index(drop=True)
+    im['charging_time'] = (1 - im['station_energy']) * car_info['capacity'][im['car_type']].reset_index(drop=True) / dsg.charging_speed * 3600
+    im['max_kilometers'] = car_info['capacity'][im['car_type']].reset_index(drop=True)*im['entry_energy'] / (pd.Series(car_info.lookup(im['car_type'],im['speed']))/1000)
+    im = im[im['station_energy'] > 0.].reset_index(drop=True)
+
     collision_matrix = np.zeros((im.shape[0], im.shape[0]))
     #TODO: check if it is possible for a car to go to that station
     #TODO: add multiple chargers in each station
@@ -18,14 +38,17 @@ def instance_to_matrix(instance, nodes, stations):
         for index2, row2 in im.iterrows():
             if index2 <= index1:
                 continue
-            if abs(row1['time_of_arrival'] - row2['time_of_arrival']) < 100:
-                collision_matrix[index1][index2] = collision_matrix[index2][index1] = 1
+            # first to come should be row1
+            if row2['station_timestamp'] < row1['station_timestamp']:
+                row1, row2 = row2, row1
 
-    return collision_matrix
+            if row2['station_timestamp'] - row1['station_timestamp'] < row1['charging_time']:
+                collision_matrix[index1][index2] = collision_matrix[index2][index1] = 1
+    return collision_matrix, im
 
 def matrix_to_bqm(cars: pd.DataFrame, conflict_matrix: np.ndarray):
     conflict_strength = 1
-    onehot_strength = 1
+    onehot_strength = 2
     H: Base = 0
     variables = {}
     for id in cars.index:
@@ -41,7 +64,7 @@ def matrix_to_bqm(cars: pd.DataFrame, conflict_matrix: np.ndarray):
     for i, idi in enumerate(cars.index):
         for j, idj in enumerate(cars.index):
             # is upper-triangular matrix guaranteed or should we check both triangles?
-            if j <= j:
+            if j <= i:
                 continue
             if conflict_matrix[i][j]:
                 H += variables[idi] * variables[idj] * conflict_strength
@@ -49,8 +72,7 @@ def matrix_to_bqm(cars: pd.DataFrame, conflict_matrix: np.ndarray):
     model = H.compile()
     return model.to_bqm()
 
-def solve_bqm(bqm):
+def bqm_to_sampleset(bqm):
     sampler = SimulatedAnnealingSampler()
-    sampleset = sampler.sample(bqm, num_reads=1000)
-    first_sample = sampleset.first.sample
-    print(first_sample)
+    sampleset = sampler.sample(bqm, num_reads=10000)
+    return sampleset

datasetgenerator.py
@@ -63,7 +63,7 @@ class DatasetGenerator:
             car_consumption = np.random.randint(min_consumption, max_consumption+1, len(self.available_car_speeds))
             self.car_types_consumptions.append(np.sort(car_consumption))
 
-        self.car_info = pd.DataFrame(np.hstack([self.car_types_battery.reshape(-1, 1), np.asarray(self.car_types_consumptions)]), columns=['capacity'] + list(map(str, self.available_car_speeds)))
+        self.car_info = pd.DataFrame(np.hstack([self.car_types_battery.reshape(-1, 1), np.asarray(self.car_types_consumptions)]), columns=['capacity'] + list(self.available_car_speeds))
         return self.car_info
 
     def generate_car_instances(self, density=3.0, sim_length=1.0, n_cars=None, sim_seconds=None):
@@ -98,7 +98,7 @@ class DatasetGenerator:
                    ('entry_node_index', int),
                    ('entry_timestamp', int),
                    ('exit_node_index', int),
-                   ('initial_energy', float)
+                   ('entry_energy', float)
                    ]
         instance = pd.DataFrame(selected_cars, columns=[x[0] for x in columns])
         self.instance = instance.astype(dtype={x:y for (x, y) in columns})